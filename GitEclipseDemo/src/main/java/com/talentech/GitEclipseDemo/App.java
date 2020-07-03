2bededeb966285fac0e2459eef5dace1475143c9
Mahbuba Alkhazra Nimme <mahbuba102@gmail.com>
2020-07-02 21:04:46 -0400

    Added a new for loop in App class

--- GitEclipseDemo/src/main/java/com/talentech/GitEclipseDemo/App.java
+++ GitEclipseDemo/src/main/java/com/talentech/GitEclipseDemo/App.java
@@ -11,5 +11,15 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World1!" );
         System.out.println( "Hello World2!" );
+        
+        for(int i = 0 ; i<10;i++){
+        	System.out.println("I am happy");
+        	
+        }
+        
+        for(int i = 0 ; i<10;i++){
+        	System.out.println("I am happy");
+        	
+        }
     }
 }
